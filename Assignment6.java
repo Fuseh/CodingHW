169b57020899e5799fbd5ff1710d23826a141740
Fuseh <92656627+Fuseh@users.noreply.github.com>
2021-12-01 21:24:55 -0500

    Add files via upload

--- Assignment6.java
+++ Assignment6.java
@@ -3,13 +3,26 @@ import java.util.Scanner;
 public class Assignment6{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter any integer and I will print that integer to zero in reverse order!");
+        System.out.println("Enter any integer and I will count down from that integer to zero!");
 
         int userInput = sc.nextInt();
         for(int i = userInput; i >= 0; i--){
             System.out.print(i);
             System.out.print(" ");
         }
+        System.out.println();
+
+        System.out.println("Cool! Enter another number and I will count down from that number to zero.");
+        userInput = sc.nextInt();
+
+        int i = userInput;
+        while(i >= 0){
+            System.out.print(i);
+            System.out.print(" ");
+            i--;
+        }
+        System.out.println();
+        System.out.println("Thank you for your time! :]");
 
     }
 }
