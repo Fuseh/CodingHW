b99b12b5cbf36e98a9815da06d51e1cc8505fe9f
Fuseh <92656627+Fuseh@users.noreply.github.com>
2021-12-03 09:30:30 -0500

    Add files via upload

--- Assignment8.java
+++ Assignment8.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Assignment8 {
-    public static void main(String[] args) {
+public class Assignment8{
+    public static void main(String[] args){
         System.out.println("********************************************************************");
         //Initializing the array list for the countries.
         ArrayList<String> Countries = new ArrayList<>();
@@ -34,15 +34,13 @@ public class Assignment8 {
 
         String userInput = sc.nextLine();
         //Searches through the Countries ArrayList and stops as soon as it finds an element that matches with the user input.
-        //Stores the index at which it found the user input and then looks for the element in the Capitals ArrayList which matches that input.
+        //Stores the index at which it found the user input and then looks for the element in the Capitals ArrayList in order to print the respective Capital City.
         //Includes an error handling "else if" statement which exists in case the user enters an invalid country.
+        boolean countryFlag = false;
         for (int i = 0; i < Countries.size(); i++) {
             if (userInput.equals(Countries.get(i))) {
                 System.out.println("The capital of " + Countries.get(i) + " is " + Capitals.get(i) + ".");
-                break;
-            }else if(!userInput.equals(Countries.get(i))){
-                System.out.println("I do not recognize that country. Valid countries are the following: " + Countries);
-                //The command below tells the for loop to not run the line above 8 times.
+                countryFlag = true;
                 break;
             }else{
                 //The line below tells the for loop to keep going, or in technical terms, continue.
@@ -50,13 +48,10 @@ public class Assignment8 {
             }
         }
 
+            if(countryFlag == false){
+                System.out.println("I do not recognize that country. Valid countries are the following: " + Countries);
+            }
         System.out.println();
         System.out.println("********************************************************************");
     }
 }
-
-
-
-
-
-
