c0c8a942281272b1d1aca1d4f515b2510f577fdb
Fuseh <92656627+Fuseh@users.noreply.github.com>
2021-12-12 14:43:47 -0500

    Add files via upload

---
+++ Assignment12.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Assignment12{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your five favorite dishes below: ");
+        String Dishes = sc.nextLine();
+
+        String[] favDishes = new String[5];
+        favDishes[0] = Dishes;
+
+        Dishes = sc.nextLine();
+        favDishes[1] = Dishes;
+
+        Dishes = sc.nextLine();
+        favDishes[2] = Dishes;
+
+        Dishes = sc.nextLine();
+        favDishes[3] = Dishes;
+
+        Dishes = sc.nextLine();
+        favDishes[4] = Dishes;
+        System.out.println();
+        System.out.print("These are your favorite dishes: ");
+        System.out.print(Arrays.toString(favDishes));
+    }
+}
